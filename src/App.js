e85bffa998ac30c4ae6a1732dd3dc2a2168392cb
cnokoo@gmail.com <cnokoo@gmail.com>
2021-01-25 03:40:54 +0000

    useeffect

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ export default function App() {
     },
   ]);
   const addThought = (thought) => {
+	  if (!thought) return;
       setThoughts((prev) => {
         return [{
           id: generateId(),
